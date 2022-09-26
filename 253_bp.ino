b4bd3beab805c0f75a36f6a395e25d9111c89525
PastelBees <73673440+PastelBees@users.noreply.github.com>
2022-09-25 20:28:07 -0400

    Update 253_bp.ino

--- 253_bp.ino
+++ 253_bp.ino
@@ -16,31 +16,45 @@ void setup()
 {
   //  change the baud rate to 9600 if required
   Serial.begin(115200);
-  
-  //  set debounce time for both the button objects
-  
 
+  //  set debounce time for both the button objects
+  push_button1.setDebounceTime(50);
+  push_button2.setDebounceTime(50);
 }
 
 void loop()
 {
   //  loop both the push_buttons : continuous polling
-  
-
+  push_button1.loop();
+  push_button2.loop();
 
   //  check if push_button1 is pressed : store the button push count into count variable
   if (push_button1.isPressed())
   {
-    
+    count++;
+    Serial.print("Number : ");
+    Serial.println(count);
   }
 
   //  else if push_button2 is pressed : print the table of the number stored in variable count
   else if (push_button2.isPressed())
   {
     //  draft a loop to print the table
-
+    Serial.print("Table of ");
+    Serial.println(count);
+    for (int i = 1; i <= 10; i++)
+    {
+      Serial.print(count);
+      Serial.print('*');
+      Serial.print(i);
+      Serial.print('\t');
+      Serial.print('=');
+      Serial.print('\t');
+      Serial.println(count*i);
+    }
 
     //  reset the number to 0, once table is printed
+     count = 0;
   }
  
 }
